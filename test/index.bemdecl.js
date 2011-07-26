b4c0cc937b39e7e4f0348ce228e212175360ede6
sopilkar <alexander.koleda@gmail.com>
2011-07-26 18:12:58 +0300

    add test

---
+++ test/index.bemdecl.js
@@ -0,0 +1,7 @@
+exports.blocks = [
+{ name: "b-page" },
+{ name: "b-head", elems: [{ name: "logo" }] },
+{ name: "b-menu", elems: [{ name: "item", mods: [{ name: "state", vals: ["current"] }] }], mods: [{ name: "size", vals: ["small"] }] },
+{ name: "b-main" },
+{ name: "b-foot" }
+]
